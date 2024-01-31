ca5ee15c8cd3ce59f42c27415e9e5a0353dd7761
Wyatt Gensler <wyattg5@grouper.cs.colostate.edu>
2024-01-30 17:11:13 -0700

    Change client team name

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -16,7 +16,7 @@ setLogLevelIfDefault();
 
 export const LOG = ulog("App");
 
-export const CLIENT_TEAM_NAME = "t28 team name";
+export const CLIENT_TEAM_NAME = "t28 BarleyBytes";
 
 export const EARTH_RADIUS_UNITS_DEFAULT = { "miles": 3959 };
 
