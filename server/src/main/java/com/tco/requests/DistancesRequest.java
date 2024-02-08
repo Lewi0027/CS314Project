85aacac63ef953f23f2f0deb4d586aea1c6f72b1
Brendan Scheidt <brendanscheidt@gmail.com>
2024-02-07 21:21:40 -0700

    Create DistancesRequest class definition - located in package com.tco.requests - import GeographicCoordinate from misc package - import BadRequestException from misc package - Define empty abstract method defined by parent Request.java

---
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -0,0 +1,10 @@
+package com.tco.requests;
+
+import com.tco.misc.GeographicCoordinate;
+import com.tco.misc.BadRequestException;
+
+public class DistancesRequest extends Request{
+    public void buildResponse() throws BadRequestException {
+        
+    }
+}
