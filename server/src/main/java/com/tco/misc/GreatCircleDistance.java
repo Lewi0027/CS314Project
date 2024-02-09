cd7088cf3cecf72e0705888ecbf4aef5b7cfe69a
Brendan Scheidt <brendanscheidt@gmail.com>
2024-02-08 19:11:16 -0700

    Define between() in GreatCircleDistance interface - create blank method stubs for methods that implement GreatCircleDistance (Cosines, Haversine, Vincenty)

--- server/src/main/java/com/tco/misc/GreatCircleDistance.java
+++ server/src/main/java/com/tco/misc/GreatCircleDistance.java
@@ -1,5 +1,5 @@
 package com.tco.misc;
 
 public interface GreatCircleDistance {
-    
+    public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius);
 }
--- server/src/main/java/com/tco/misc/Vincenty.java
+++ server/src/main/java/com/tco/misc/Vincenty.java
@@ -1,5 +1,8 @@
 package com.tco.misc;
 
 public class Vincenty implements GreatCircleDistance{
-    
+    @Override
+    public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
+        return 0l;
+    }
 }
