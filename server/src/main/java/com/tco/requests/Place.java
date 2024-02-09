738b59523686ead8876f2e40f5584dfda540a455
“Diego <diegocel@colostate.edu>
2024-02-08 18:26:58 -0700

    Adding instance variables for place class

--- server/src/main/java/com/tco/requests/Place.java
+++ server/src/main/java/com/tco/requests/Place.java
@@ -3,5 +3,6 @@ package com.tco.requests;
 import com.tco.misc.GeographicCoordinate;
 
 public class Place implements GeographicCoordinate{
-    
+    private double latitude;
+    private double longitude;
 }
