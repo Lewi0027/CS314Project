0ebdd0df27ecf4588c621d86b887a5efd5742d47
Brendan Scheidt <brendanscheidt@gmail.com>
2024-02-08 18:43:02 -0700

    Create constructor for Place class - Set instance variables to passed in variables

--- server/src/main/java/com/tco/requests/Place.java
+++ server/src/main/java/com/tco/requests/Place.java
@@ -5,4 +5,9 @@ import com.tco.misc.GeographicCoordinate;
 public class Place implements GeographicCoordinate{
     private double latitude;
     private double longitude;
+
+    public Place(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
