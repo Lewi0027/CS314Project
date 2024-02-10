0cb8bfb9ac874aa455d6a115c7e772669a1e6329
Brendan Scheidt <brendanscheidt@gmail.com>
2024-02-10 15:14:47 -0700

    Fix return types of calculator classes - Vincenty, Haversine, Cosines, GreatCircleDistance - return Long instead of long

--- server/src/main/java/com/tco/misc/Cosines.java
+++ server/src/main/java/com/tco/misc/Cosines.java
@@ -2,7 +2,7 @@ package com.tco.misc;
 
 public class Cosines implements GreatCircleDistance{
     @Override
-    public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
+    public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
         return 0l;
     }
 }
--- server/src/main/java/com/tco/misc/Haversine.java
+++ server/src/main/java/com/tco/misc/Haversine.java
@@ -2,7 +2,7 @@ package com.tco.misc;
 
 public class Haversine implements GreatCircleDistance{
     @Override
-    public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
+    public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
         return 0l;
     }
 }
