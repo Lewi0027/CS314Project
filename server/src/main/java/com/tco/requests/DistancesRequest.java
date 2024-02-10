eb078fe85a2ad6e94121765621c29f93144caf4b
Wyatt Gensler <wyattg5@winter-park.cs.colostate.edu>
2024-02-10 14:23:06 -0700

    Added distance request constructor

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -9,6 +9,13 @@ public class DistancesRequest extends Request {
     private final Double earthRadius;
     private final String formula;
 
+    public DistancesRequest(Places places, Double earthRadius, String formula) {
+        this.places = places;
+        this.earthRadius = earthRadius;
+        this.formula = formula;
+        this.distances = new Distances();
+    }
+
     @Override
     public void buildResponse() throws BadRequestException {
         
