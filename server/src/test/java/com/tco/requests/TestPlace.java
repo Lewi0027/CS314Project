d11a45902f7f374be10f3333968062695749b6f0
leichneraj <ajlei@colostate.edu>
2024-02-10 22:47:28 -0700

    Add lonRadians() method in Place

--- server/src/test/java/com/tco/requests/TestPlace.java
+++ server/src/test/java/com/tco/requests/TestPlace.java
@@ -7,9 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Random;
+
 public class TestPlace {
 
     private Place place;
+
+    private Random random = new Random();
     
     @Test
     @DisplayName("bscheidt: latRadians() returns correct conversion of 0")
@@ -35,7 +39,39 @@ public class TestPlace {
     @Test
     @DisplayName("bscheidt: latRadians() returns correct conversion of random latitude")
     public void testLatRadiansRandom() {
-        place = new Place("37.493352", "0");
-        assertEquals((37.493352 * (Math.PI/180)), place.latRadians());
+        double randomDouble = random.nextDouble() * 180 - 90; 
+        String testValue = randomDouble + "";
+        place = new Place(testValue, "0");
+        assertEquals((randomDouble * (Math.PI/180)), place.latRadians());
+    }
+
+    @Test
+    @DisplayName("ajlei: lonRadians() returns correct conversion of 0")
+    public void testLonRadiansZero() {
+        place = new Place("0", "0");
+        assertEquals((0 * (Math.PI/180)), place.lonRadians());
+    }
+
+    @Test
+    @DisplayName("ajlei: lonRadians() returns correct conversion of 180")
+    public void testLonRadiansMax() {
+        place = new Place("0", "180");
+        assertEquals((180 * (Math.PI/180)), place.lonRadians());
+    }
+
+    @Test
+    @DisplayName("ajlei: lonRadians() returns correct conversion of -180")
+    public void testLonRadiansMin() {
+        place = new Place("0", "-180");
+        assertEquals((-180 * (Math.PI/180)), place.lonRadians());
+    }
+
+    @Test
+    @DisplayName("ajlei: lonRadians() returns correct conversion of random longitude")
+    public void testLonRadiansRandom() {
+        double randomDouble = random.nextDouble() * 360 - 180; 
+        String testValue = randomDouble + "";
+        place = new Place("0", testValue);
+        assertEquals((randomDouble * (Math.PI/180)), place.lonRadians());
     }
 }
