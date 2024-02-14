087b39dee23c50796bacd90affca30e39e40dd31
Wyatt Gensler <wyattg5@winter-park.cs.colostate.edu>
2024-02-14 10:54:22 -0700

    Completed Cosines class - closes #49

--- server/src/main/java/com/tco/misc/Cosines.java
+++ server/src/main/java/com/tco/misc/Cosines.java
@@ -1,8 +1,29 @@
 package com.tco.misc;
 
+import static java.lang.Math.*;
+
 public class Cosines implements GreatCircleDistance{
+
     @Override
     public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
-        return 0l;
+        Double latitudeFrom = from.latRadians();
+        Double longitudeFrom = from.lonRadians();
+        Double latitudeTo = to.latRadians();
+        Double longitudeTo = to.lonRadians();
+
+        Double calculatedCosine = cosines(latitudeFrom, longitudeFrom, latitudeTo, longitudeTo);
+
+        double distance = calculatedCosine * earthRadius;
+
+        return Math.round(distance);
+    }
+
+    private static double cosines(Double lat1, Double lon1, Double lat2, Double lon2){
+        Double longitudeDifference = lon2 - lon1;
+        Double sinProduct = Math.sin(lat1) * Math.sin(lat2);
+        Double cosProduct = Math.cos(lat1) * Math.cos(lat2) * Math.cos(longitudeDifference);
+        Double radianDistance = Math.acos(sinProduct + cosProduct);
+        return radianDistance;
+        
     }
 }
