4cea43ecb8ab6afe05bea910821b5a45510a2053
leichneraj <ajlei@colostate.edu>
2024-02-28 15:53:17 -0700

    Create skeleton class for OptimizerFactory.java

---
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -0,0 +1,9 @@
+package com.tco.misc;
+
+public class OptimizerFactory {
+
+    public static TourOptimizer createOptimizer(int placesSize, Double response) {
+        return null; // Not needed after full implementation of method
+    }
+
+}
