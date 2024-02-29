24140be3961043c088ca1306a7b936272398b7d3
Brendan Scheidt <brendanscheidt@gmail.com>
2024-02-28 20:29:39 -0700

    Add data structures to TourOptimizer - create long[][] distanceMatrix - create int[] tour

--- server/src/main/java/com/tco/misc/TourOptimizer.java
+++ server/src/main/java/com/tco/misc/TourOptimizer.java
@@ -4,6 +4,9 @@ import com.tco.requests.Places;
 
 public abstract class TourOptimizer {
 
+    private long[][] distanceMatrix;
+    private int[] tour;
+
     public Places construct(Places places, Double earthRadius, String formula, Double response){
         return places;
     }
