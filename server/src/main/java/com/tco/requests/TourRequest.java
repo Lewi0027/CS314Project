d504d84f2735f1434986a4cac4fac9be2073412a
Brendan Scheidt <brendanscheidt@gmail.com>
2024-03-04 11:50:36 -0700

    Connect optimizer to TourRequest - buildResponse contacts OptimizerFactory for optimizer - sets this.places to be the result of NN algo from optimizer - tests check 0, 1, and multiple places modify this.places correctly

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -3,6 +3,10 @@ package com.tco.requests;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.tco.misc.OptimizerFactory;
+import com.tco.misc.TourOptimizer;
+import com.tco.misc.OneOpt;
+
 import java.util.ArrayList;
 
 public class TourRequest extends Request{
@@ -23,6 +27,17 @@ public class TourRequest extends Request{
 
     @Override
     public void buildResponse() {
+        constructPlacesWithOptimizer();
         log.trace("buildResponse -> {}", this);
+    }
+
+    protected void constructPlacesWithOptimizer() {
+        TourOptimizer optimizer = OptimizerFactory.createOptimizer(this.places.size(), this.response);
+        this.places = optimizer.construct(this.places, this.earthRadius, this.formula, this.response);
+    }
+
+    // for testing
+    protected Places getPlaces() {
+        return this.places;
     }
 }
