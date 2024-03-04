da42bc440015f640fae7c010054e65aef72717d5
Wyatt Gensler <wyattg5@winter-park.cs.colostate.edu>
2024-03-04 13:27:16 -0700

    Wyattg5 deifne improve() TwoOpt - closes #163

--- server/src/main/java/com/tco/misc/TwoOpt.java
+++ server/src/main/java/com/tco/misc/TwoOpt.java
@@ -4,6 +4,40 @@ public class TwoOpt extends TourOptimizer{
     
     @Override
     public void improve() {
-        //Implement improve method here.
+        int tourLength = tour.length;
+        boolean improvement = true;
+        while (improvement) {
+            improvement = false;
+            for(int i = 0; i <= tourLength - 3; i++){
+                for(int k = 0; k <= tourLength - 1; k++){
+                    if(isImproved(tour, i, k)){
+                        swapIndex(tour, i, k);
+                        improvement = true;
+                    }
+                }
+            }
+        }
     }
+    private boolean isImproved(int[] tour, int i, int k){
+        //calculate if new leg distance swapping is shorter than current leg distance
+        //return (newLeg < oldLeg);
+        return true;
+    }
+
+    private void swapIndex(int[] tour, int i1, int k){
+        //Reverse the places in the tour without making new data structure
+
+        /*
+        Dave's pseudo code:
+
+        while(i1 < k){
+            temp = tour[i1]
+            tour[i1] = tour[k]
+            tour[k] = temp
+            i1++
+            k--
+        }
+        */
+    }
+
 }
