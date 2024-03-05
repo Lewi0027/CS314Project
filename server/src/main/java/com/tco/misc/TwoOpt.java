bf9904b8b5f019ece3769eea0ce33eab7a7eb67d
Wyatt Gensler <wyattg5@hornet.cs.colostate.edu>
2024-03-04 18:00:20 -0700

    Added route array and fixed legs accessing distance matrix

--- server/src/main/java/com/tco/misc/TwoOpt.java
+++ server/src/main/java/com/tco/misc/TwoOpt.java
@@ -1,29 +1,35 @@
 package com.tco.misc;
 
+import java.util.Arrays;
+
 public class TwoOpt extends TourOptimizer{
     
     @Override
     public void improve() {
-        int tourLength = tour.length;
+        int[] route = Arrays.copyOf(tour, tour.length + 1);
+        route[route.length - 1] = tour[0];
         boolean improvement = true;
+        
         while (improvement) {
             improvement = false;
-            for(int i = 0; i <= tourLength - 3; i++){
-                for(int k = 0; k <= tourLength - 1; k++){
+            for(int i = 0; i <= route.length - 3; i++){
+                for(int k = 2; k <= route.length - 1; k++){
                     if(isImproved(tour, i, k)){
-                        swapIndex(tour, i, k);
+                        swapIndex(route, i, k);
                         improvement = true;
                     }
                 }
             }
         }
+        this.tour = Arrays.copyOf(route, route.length - 1);
     }
-    
+
+
     private boolean isImproved(int[] tour, int i, int k){
-        long newLegStart = distanceMatrix[i][k];
-        long newLegEnd = distanceMatrix[i+1][k+1];
-        long oldLegStart = distanceMatrix[i][i+1];
-        long oldLegEnd = distanceMatrix[k][k+1];
+        long newLegStart = distanceMatrix[tour[i]][tour[k]];
+        long newLegEnd = distanceMatrix[tour[i+1]][tour[k+1]];
+        long oldLegStart = distanceMatrix[tour[i]][tour[i+1]];
+        long oldLegEnd = distanceMatrix[tour[k]][tour[k+1]];
 
         long newDistance = newLegStart + newLegEnd;
         long oldDistance = oldLegStart + oldLegEnd;
@@ -31,7 +37,7 @@ public class TwoOpt extends TourOptimizer{
         return newDistance < oldDistance;
     }
 
-    private void swapIndex(int[] tour, int i1, int k){
+    private void swapIndex(int[] route, int i1, int k){
         //Reverse the places in the tour without making new data structure
 
         /*
