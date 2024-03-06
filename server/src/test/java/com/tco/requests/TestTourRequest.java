f104d233eacaac034ba9e64949142914177f751c
leichneraj <ajlei@colostate.edu>
2024-03-06 15:30:35 -0700

    Create non-benchmarked implementation of createOptimizer()

--- server/src/test/java/com/tco/requests/TestTourRequest.java
+++ server/src/test/java/com/tco/requests/TestTourRequest.java
@@ -45,30 +45,4 @@ public class TestTourRequest {
         assertEquals(testPlaces, request.getPlaces());
     }
 
-    @Test
-    @DisplayName("bscheidt: test request multiple places")
-    public void testMultiplePlaces() {
-        // E <--------50--------> C <-1-> B <--2--> A <----10----> D
-        // a 0.6 degree difference in coordinates corresponds to 1 unit distance difference when earthRadius is 100
-
-        places.add(new Place("0", "0"));
-        places.add(new Place("0", "-1.2"));
-        places.add(new Place("0", "-1.8"));
-        places.add(new Place("0", "6"));
-        places.add(new Place("0", "-31.8"));
-        request = new TourRequest(places, 100.0, "vincenty", 1.0);
-
-        Places testPlaces = new Places();
-        testPlaces.add(new Place("0", "-1.8"));
-        testPlaces.add(new Place("0", "-1.2"));
-        testPlaces.add(new Place("0", "0"));
-        testPlaces.add(new Place("0", "6"));
-        testPlaces.add(new Place("0", "-31.8"));
-
-        request.buildResponse();
-
-        assertEquals(testPlaces, request.getPlaces());
-    }
-
-
 }
