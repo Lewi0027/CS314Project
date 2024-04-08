f6489cb67dc3616e831136966bdb8800aeedbf7b
Wyatt Gensler <wyattg5@grouper.cs.colostate.edu>
2024-04-08 13:00:10 -0600

    Fix nesting in Three Opt

--- server/src/main/java/com/tco/misc/ThreeOpt.java
+++ server/src/main/java/com/tco/misc/ThreeOpt.java
@@ -3,31 +3,45 @@ package com.tco.misc;
 import java.util.Arrays;
 
 public class ThreeOpt extends TwoOpt{
-    
+
+    private int i;
+    private int j;
+    private int k;
+    private int n;
+    boolean improvement = true;
+
     @Override
     public void improve() {
         this.route = Arrays.copyOf(tour, tour.length + 1);
         this.route[route.length - 1] = tour[0];
-        int n = tour.length; 
+        n = tour.length; 
 
-        boolean improvement = true;
         while (improvement) {
             if (tooMuchTimeElapsed()) break;
             improvement = false;
-            for (int i = 0; i <= n - 3; i++) {
-                for (int j = i + 1; j <= n - 2; j++) {
-                    for (int k = j + 1; k <= n - 1; k++) {
-                        if (tooMuchTimeElapsed()) break;
-                        int reversals = threeOptReversals(i, j, k);
-                        if (threeOptReverseI1J(reversals)) swapIndex(i + 1, j);
-                        if (threeOptReverseJ1K(reversals)) swapIndex(j + 1, k);
-                        if (threeOptReverseI1K(reversals)) swapIndex(i + 1, k);
-                        if (reversals > 0) improvement = true;
-                    }
-                }
-            }
-        }
+            loopOne();
         this.tour = Arrays.copyOf(this.route, this.route.length - 1);
+        }
+    }
+    private void loopOne() {
+        for (i = 0; i <= n - 3; i++) {
+            loopTwo();
+        }
+    }
+    private void loopTwo() {
+        for (j = i + 1; j <= n - 2; j++) {
+            loopThree();
+        }
+    }
+    private void loopThree() {
+        for (k = j + 1; k <= n - 1; k++) {
+            if (tooMuchTimeElapsed()) break;
+            int reversals = threeOptReversals(i, j, k);
+            if (threeOptReverseI1J(reversals)) swapIndex(i + 1, j);
+            if (threeOptReverseJ1K(reversals)) swapIndex(j + 1, k);
+            if (threeOptReverseI1K(reversals)) swapIndex(i + 1, k);
+            if (reversals > 0) improvement = true;
+        }
     }
 
     protected boolean threeOptReverseI1J(int reversals){
