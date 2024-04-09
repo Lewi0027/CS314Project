8e87c88d622c76e3e52b8eaaf14d00b2528fa0a0
Brendan Scheidt <brendanscheidt@gmail.com>
2024-04-08 18:55:57 -0600

    Remove tests checking invalid request - schema will catch these tests before they hit the server

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -43,7 +43,7 @@ public class DistancesRequest extends Request {
     }
 
     private boolean isInvalidFormula() {
-        return !formula.equalsIgnoreCase("Vincenty") && !formula.equalsIgnoreCase("Haversine") && !formula.equalsIgnoreCase("Cosines")
+        return !formula.equalsIgnoreCase("Vincenty") && !formula.equalsIgnoreCase("Haversine") && !formula.equalsIgnoreCase("Cosines");
     }
 
     private Distances buildDistanceList(){
