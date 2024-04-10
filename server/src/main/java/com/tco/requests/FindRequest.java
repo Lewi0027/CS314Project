dc04b83aae48523b3c0f398d436d3e31f293291a
Henry Lewis <lewi0027@harrisburg.cs.colostate.edu>
2024-04-10 15:49:35 -0600

    Henry Lewis closing out issue#414

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -8,18 +8,36 @@ import com.tco.misc.TourOptimizer;
 import com.tco.misc.OneOpt;
 import com.tco.query.NearLocations;
 import com.tco.misc.BadRequestException;
+import com.tco.query.FindLocations;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class FindRequest extends Request{
 
+    private Places places = new Places();
+    String match;
+    List<String> type;
+    List<String> where;
+    int limit;
+
+
     private final static transient Logger log = LoggerFactory.getLogger(FindRequest.class);
+
+    public FindRequest(String match, List<String> type, List<String> where, Integer limit) {
+        this.match = match;
+        this.type = type;
+        this.where = where;
+        this.limit = limit;
+    }
     
     @Override
     public void buildResponse() throws BadRequestException{
         try {
-            
+            FindLocations locations = new FindLocations(this.match, this.type, this.where, this.limit);
+            this.places = locations.find();
+            this.limit = locations.found();
         } 
         catch (Exception e) {
             log.error("Error processing request: ", e);
