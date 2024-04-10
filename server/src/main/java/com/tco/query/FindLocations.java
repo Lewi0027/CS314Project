650bcef08a97d6825e906e099ad0ae79d746a942
“Diego <diegocel@colostate.edu>
2024-04-10 13:02:53 -0600

    Filled FindLocations class skeleton

--- server/src/main/java/com/tco/query/FindLocations.java
+++ server/src/main/java/com/tco/query/FindLocations.java
@@ -1,5 +1,32 @@
 package com.tco.query;
 
+import com.tco.query.Select;
+import com.tco.query.Database;
+import com.tco.misc.BadRequestException;
+import com.tco.requests.Places;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FindLocations {
-    
+    Places places;
+    String match;
+    List<String> type;
+    List<String> where;
+    int limit;
+
+    public FindLocations(String match, List<String> type, List<String> where, int limit){
+        this.match = match;
+        this.type = type;
+        this.where = where;
+        this.limit = limit == 0 ? 100000 : limit;
+    }
+
+    public Places find() throws Exception {
+        
+    }
+
+    public Integer found() throws Exception {
+
+    }
 }
